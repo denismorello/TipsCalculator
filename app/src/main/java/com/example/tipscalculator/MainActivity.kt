a519875a4f601770a9372070e09085924d5f7c71
Denis <denismorello@gmail.com>
2024-06-04 22:36:30 -0300

    Calculadora de gorjetas

---
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.tipscalculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    //Desenvolvimentos a serem realizados:
+    //Valor total da conta
+    //Numero de pessoas
+    //Porcentagem da gorjeta
+        //10%, 15% ou 20%
+    //Calular
+    //Limpar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
