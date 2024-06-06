69466853c99bce49cf19639193357be8fb2d24de
Denis <denismorello@gmail.com>
2024-06-05 23:07:45 -0300

    Implementação do ViewBinding

--- app/src/main/java/com/example/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.tipscalculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tipscalculator.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,14 +20,31 @@ class MainActivity : AppCompatActivity() {
     //Calular
     //Limpar
 
+    //Recuperar as views do layout
+        //ViewBinding
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val tieTotal: TextInputEditText = findViewById(R.id.tie_total) //Posso declarar o tipo após a variável
+        val tieNumberOfPeople: TextInputEditText = findViewById(R.id.tie_number_of_people)
+        val btnClean = findViewById<Button>(R.id.btn_clean) //posso declarar o tipo no findViewById
+        val btnCalculate = findViewById<Button>(R.id.btn_calculate)
+
+        binding.btnClean.setOnClickListener {
+            println()
+        }
+
     }
 }
